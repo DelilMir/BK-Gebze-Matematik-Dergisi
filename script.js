c67dc959e1e6b94aeea66a2bfc3d2d21631245de
DelilMir <117032152+DelilMir@users.noreply.github.com>
2023-02-03 22:27:30 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const arrows = document.querySelectorAll(".arrows");
+const movieList = document.querySelectorAll(".movie-list");
+
+
+arrows.forEach((arrow, i)=> {
+    let clickCounter = 0
+    arrow.addEventListener("click", function(){
+        clickCounter++;
+        movieList[i].style.transform = `translateX(${movieList[i].computedStyleMap().get("transform")[0].x.value- 300}px)`
+    })
+})
